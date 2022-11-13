3383d5dcc49601cdd9b51d25f337e98ad6728501
rotatingcow <ben.arkonovich@gmail.com>
2022-11-13 10:29:29 -0800

    Added piece sprites and mouse sensing

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -1,19 +1,18 @@
 import javax.swing.*;
-import java.awt.*;
+
+//import java.awt.*;
 
 public class AlphaBetaChess {
 
     public static void main(String[] args) {
         JFrame gameFrame = new JFrame("Chess");
-        
-        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        GUI ui = new GUI();
-        ui.paintComponents(null);
-        gameFrame.add(ui);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         gameFrame.setSize(750,750);
-        gameFrame.setVisible(true);
-       
+
+        
+        GUI ui = new GUI();
+        gameFrame.add(ui);
+        gameFrame.setVisible(true); 
     }
     
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,69 @@
 
 import java.awt.*; 
 import javax.swing.*;
-public class GUI extends JPanel{
+import javax.swing.event.*;
 
-    public void paintComponents(Graphics graphic){
-        graphic.setColor(Color.RED);
-        graphic.fillRect(20, 10, 20, 10);
+public class GUI extends JPanel implements MouseInputListener {
+    int x=15 , y=15;
+
+    public void paint(Graphics graphic){
+        graphic.setColor(Color.BLUE);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        Image rookWhite = new ImageIcon("/Users/benarkonovich/Documents/GitHub/Chess/images/rook_white.png").getImage();
+        
+        //graphic.fillRect(x, y, 50, 50);
+
+        for(int c = 1; c< 9; c++){
+            for(int r = 1; r < 9; r++){
+                if((r+c) % 2 == 0){
+                    graphic.setColor(new Color(255,248,238));
+                }
+                else{
+                    graphic.setColor(new Color(102, 61, 20));
+                }
+                graphic.fillRect(r*50+150, c*50+150, 50, 50);
+            }
+        }
+        graphic.drawImage(rookWhite, x, y, this);
     }
     
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent e) {
+        
+        
+    }
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void mouseExited(java.awt.event.MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void mouseDragged(java.awt.event.MouseEvent e) {
+        x = e.getX();
+        y= e.getY();
+        repaint();
+        
+    }
+    @Override
+    public void mouseMoved(java.awt.event.MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
