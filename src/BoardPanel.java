df9344da66000d38517ed54ccd16fba385132649
BArkonovich7102 <ben.arkonovich@gmail.com>
2022-11-18 15:50:14 -0800

    Restore files

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,14 +1,53 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.awt.*;
 import javax.swing.*;
+import Pieces.*;
+
 
 public class BoardPanel extends JPanel{
-    final List<TilePanel> boardTiles = null;
+    List<TilePanel> boardTiles = null;
 
-    public BoardPanel(){
+    public BoardPanel(char[][] board){
         super(new GridLayout(8,8));
-        //this.boardTiles = new ArrayList<>();
+        this.boardTiles = new ArrayList<TilePanel>();
+        
+        for(int r = 0; r < board.length; r++){
+
+            for(int c=0; c < board[r].length ; c++){
+                int row = r+1;
+                int col = c+1;
+                char currentChar = board[r][c];
+                switch (currentChar){
+                    case ' ': this.boardTiles.add(new TilePanel(row+col));
+                        break;
+                    case 'P': this.boardTiles.add(new TilePanel(new Pawn(true),row+col));
+                        break;
+                    case 'R': this.boardTiles.add(new TilePanel(new Rook(true),row+col));
+                        break; 
+                    case 'N': this.boardTiles.add(new TilePanel(new Knight(true),row+col));
+                        break;  
+                    case 'B': this.boardTiles.add(new TilePanel(new Bishop(true),row+col));
+                        break;   
+                    case 'Q': this.boardTiles.add(new TilePanel(new Queen(true),row+col));
+                        break; 
+                    case 'K': this.boardTiles.add(new TilePanel(new King(true),row+col));
+                        break; 
+                    case 'p': this.boardTiles.add(new TilePanel(new Pawn(false),row+col));
+                        break;
+                    case 'r': this.boardTiles.add(new TilePanel(new Rook(false),row+col));
+                        break; 
+                    case 'n': this.boardTiles.add(new TilePanel(new Knight(false),row+col));
+                        break;  
+                    case 'b': this.boardTiles.add(new TilePanel(new Bishop(false),row+col));
+                        break;   
+                    case 'q': this.boardTiles.add(new TilePanel(new Queen(false),row+col));
+                        break; 
+                    case 'k': this.boardTiles.add(new TilePanel(new King(false),row+col));
+                        break;
+                }
+            }
+        }
     }
     
 }
--- src/TilePanel.java
+++ src/TilePanel.java
@@ -1,5 +1,33 @@
-import javax.swing.JPanel;
+import javax.swing.*;
+
+import Pieces.Piece;
+
 
 public class TilePanel extends JPanel{
+    boolean Islight;
+    boolean empty;
+    Piece piece;
+    int num;
+    
+    public TilePanel(Piece piece, int index){
+        this.Islight = index-(index/8) % 2 == 0 ? true: false;
+        this.num = index;
+        this.empty = false;
+        this.piece = piece;
+
+
+    }
+
+    public TilePanel(int index){
+        this.Islight = index-(index/8) % 2 == 0 ? true: false;
+        this.num = index;
+        this.empty = true;
+        
+
+
+        
+    }
+
+
     
 }
